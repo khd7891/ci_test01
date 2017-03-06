32cc19f138badd153554a6271d23f3f0ccc42310
HD <khd7891@hanmail.net>
2017-03-06 13:39:47 +0900

    프로젝트 공유

---
+++ asdhfkashdkjgvaj/src/main/java/com/bitacademy/maven/HelloMaven.java
@@ -0,0 +1,16 @@
+package com.bitacademy.maven;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("hellomaven")
+public class HelloMaven {
+
+	@RequestMapping("hello.do")
+	public String hello(){
+		return "Hello.jsp"; 
+			
+	}
+	
+	
+}
